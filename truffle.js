89d664c05b1a10c73caa677b4f3ed6b908b25421
Rohit Shukla <rohitshukla556@gmail.com>
2017-12-15 15:38:56 +0530

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -4,9 +4,9 @@ require('babel-register')
 module.exports = {
   networks: {
     development: {
-      host: 'localhost',
+      host: "localhost",
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: "*" // Match any network id
     }
-  }
-}
+  }  
+};
